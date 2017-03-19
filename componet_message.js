fade9dd6737e69f42ace98cc128a5faab2ab5926
Alexey Dobrynin <ag.dobrynin@gmail.com>
2017-03-19 22:39:58 +0400

    Add fix for template :class="this.alertclass" class="alert alert-info"

--- componet_message.js
+++ componet_message.js
@@ -1,17 +1,36 @@
 Vue.component('message',{
-    props:['title','body','alertclass'],
-    template:`<div class="jumbotron"><button type="button" class="close"><span>&times;</span></button>
-    <h1>{{ title }}</h1><p>{{ body }}</p></div>`
+    props:['title','body'],
+    data(){
+        return{
+            isVisible: true
+        }
+    },
+    template:`<div class="jumbotron" v-show="isVisible"><button type="button" @click="isVisible = false" class="close"><span>&times;</span></button>
+    <h1>{{ title }}</h1><p>{{ body }}</p></div>`,
 });
 
 Vue.component('alert',{
     props:['title', 'body', 'alertclass'],
-    mounted() {
-        console.log(this.alertclass);
+    data(){
+        return{
+            isVisible: true
+        }
     },
-    template:`<div class="alert alert-info">
-    <button type="button" class="close"><span>&times;</span></button>
-    <strong>{{ title }}</strong> {{ body }}</div>`
+    mounted() {
+
+    },
+    methods:{
+        hide(){
+            this.isVisible = false
+        }
+    },
+    template:`<div :class="this.alertclass" class="alert alert-info" v-show="isVisible">
+    <button type="button" class="close" @click="isVisible = false"><span>&times;</span></button>
+    <strong>{{ title }}</strong> {{ body }}
+    <div class="container">
+        <div><button type="button" class="btn btn-default" @click="hide">закрыть</button></div>
+    </div>
+    </div>`
 });
 
 let app = new Vue({
