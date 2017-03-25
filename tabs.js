27512badc99672a844621452bcc34f318b184ade
Alexey Dobrynin <ag.dobrynin@gmail.com>
2017-03-25 17:25:06 +0400

    Lesson 11.

---
+++ tabs.js
@@ -0,0 +1,69 @@
+Vue.component('tabs', {
+    data(){
+        return { tabs: [] };
+    },
+    template: `
+    <div>
+        <div class="tabs">
+          <ul>
+            <li v-for="tab in tabs" :class="{ 'is-active' : tab.isActive }">
+                <a :href="tab.href" @click.prevent="selectTab($event, tab)">{{ tab.name }}</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>
+    `,
+    created(){
+        this.tabs = this.$children;
+    },
+    mounted() {
+
+    },
+    methods:{
+        selectTab( event , selectedTab ){
+            this.tabs.forEach( function( tab ){
+                tab.isActive = (tab.href == selectedTab.href);
+            });
+        }
+    }
+});
+
+Vue.component('tab', {
+    props:{
+        name: {
+            required: true
+        },
+        selected:{
+            default : false
+        }
+    },
+    data(){
+        return {
+            isActive: false
+        }
+    },
+    template: `
+        <div v-show="isActive">
+            <slot></slot>
+        </div>
+    `,
+    mounted() {
+        this.isActive = this.selected;
+    },
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '_');;
+        }
+    },
+});
+
+new Vue({
+    el: "#root",
+    mounted() {
+
+    }
+});
